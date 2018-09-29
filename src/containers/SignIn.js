59992d976ea59192e60693a0c9f0e32f4b06d35b
Mario Peñate Fariñas <ckwanted@gmail.com>
2018-09-29 01:37:36 +0100

    Fix SignIn component

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -26,46 +26,36 @@ class SignIn extends Component {
     }
 
     state = {
-        idCard: this.props.value,
-        password: this.props.value,
+        idCard: '',
+        password: '',
         idCardErrorMessage: null,
         passwordErrorMessage: null,
     }
 
-    _idCardErrorMessage() {
-        if(!this.state.idCard || this.state.idCard.length <=0){
-            this.setState({idCardErrorMessage: strings('signinErrorMessage')});
-        }else {
-            this.setState({idCardErrorMessage: null});
-        }
+    _isValidCard = () => {
+        const {idCard} = this.state;
+
+        return !idCard || idCard.length === 0 ? false : true;
     }
 
-    _passwordErrorMessage() {
-        if(!this.state.password || this.state.password.length <=5){
-            this.setState({passwordErrorMessage: strings('signinErrorMessage')});
-        }else {
-            this.setState({passwordErrorMessage: null});
-        }
+    _isPassword = () => {
+        const {password} = this.state;
+
+        return !password ||password.length === 0 ? false : true;
     }
 
-    getDisabled() {
-        let disabled = false;
-        
-        if(!this.state.idCard || this.state.idCard.length <=0){
-            disabled = true;
-        }
-            
-        if(!this.state.password || this.state.password.length <=5){
-            disabled = true;
-        }
-        return disabled;
+    _handleIdCardError = () => {
+        if(!this._isValidCard()) this.setState({idCardErrorMessage: strings('signinErrorMessage') });
+        else this.setState({idCardErrorMessage: null});
     }
 
-    toggleError() {
-
+    _handlePasswordError = () => {
+        if(!this._isPassword()) this.setState({passwordErrorMessage: strings('signinErrorMessage')});
+        else this.setState({passwordErrorMessage: null});
     }
 
     render() {
+
         return(
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 
@@ -76,10 +66,11 @@ class SignIn extends Component {
                     labelStyle={styles.text}
                     placeholder={strings('idCard')}
                     onChangeText={(idCard) => {
-                        this.setState({idCard})
-                        this._idCardErrorMessage()
+                        this.setState({idCard}, () => {
+                            this._handleIdCardError()
+                        })
                     }}
-                    value={this.state.id_card}
+                    value={this.state.idCard}
                     inputStyle={styles.textInput}
                     leftIcon={
                         <Icon
@@ -98,8 +89,9 @@ class SignIn extends Component {
                     placeholder={strings('Password')}
                     secureTextEntry={true}
                     onChangeText={(password) => {
-                        this.setState({password})
-                        this._passwordErrorMessage()
+                        this.setState({password}, () => {
+                            this._handlePasswordError();
+                        })
                     }}
                     value={this.state.password}
                     inputStyle={styles.textInput}
@@ -115,7 +107,7 @@ class SignIn extends Component {
                 />
 
                 <Button
-                    disabled={this.getDisabled()}
+                    disabled={this.state.idCard.length === 0 || this.state.password.length === 0}
                     title={strings('SIGN_IN')}
                     backgroundColor={Color.grey}
                     onPress={() => this.props.navigation.navigate('TermsConditionsWelcome')}
