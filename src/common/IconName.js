8fd0435d236cf7e8f2dc2f1f7e7067acbf218294
dlrjad <jorgdelga@hotmail.com>
2018-09-30 20:43:05 +0100

    Translated words & defined colors

--- src/common/IconName.js
+++ src/common/IconName.js
@@ -5,6 +5,9 @@ export default {
     SEARCH: Platform.OS === 'ios' ? 'ios-search' : 'ios-search',
     CART: Platform.OS === 'ios' ? 'ios-cart' : 'ios-cart',
     SETTINGS: Platform.OS === 'ios' ? 'ios-settings' : 'ios-settings',
-    USER: Platform.OS === 'ios' ? 'person' : 'person',
+    BUSINESS: Platform.OS === 'ios' ? 'business' : 'business',
+    CHEVRON_RIGHT: Platform.OS === 'ios' ? 'chevron-right' : 'chevron-right',
     PASSWORD: Platform.OS === 'ios' ? 'vpn-key' : 'vpn-key',
+    PHONE: Platform.OS === 'ios' ? 'phone-android' : 'phone-android',
+    USER: Platform.OS === 'ios' ? 'person' : 'person',
 }
